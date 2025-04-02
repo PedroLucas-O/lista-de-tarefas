20720ac87d562ad7aa1850bceed9eec0df02f184
PedroLucas-O <pedrolucas.pc.cp@gmail.com>
2025-04-02 10:30:40 -0300

    CSS do projeto

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,24 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import "./lista-de-tarefas.css"
 
 export default function Home () {
     const [tarefas, setTarefas] = useState([])
     const [novaTarefa, setNovaTarefa] = useState("")
     const [filtro, setFiltro] = useState("")
+    
+    useEffect(() => {
+        const tarefasSalvas = JSON.parse(localStorage.getItem('tarefas')) || [] 
+        if (Array.isArray(tarefasSalvas)) {
+            setTarefas(tarefasSalvas)
+        }        
+    }, [])
+
+    useEffect(() => {
+        if (tarefas.length>0) {
+            localStorage.setItem('tarefas', JSON.stringify(tarefas))
+        }
+    }, [tarefas])
+
     const tarefasFiltradas = filtro?tarefas.filter((tarefa) => tarefa.toLowerCase().includes(filtro.toLowerCase())):[]
 
     function adicionarTarefa () {
@@ -12,23 +27,30 @@ export default function Home () {
         setNovaTarefa("")
     }
 
+    function removerTarefa (index) {
+        const novasTarefas = tarefas.filter((_, i) => i !== index)
+        setTarefas(novasTarefas)
+        localStorage.setItem('tarefas', JSON.stringify(novasTarefas))
+    }
+
     return (
-        <div>
-            <h1>Lista de Tarefas</h1>
-            <div>
+        <div className="container">
+            <h1 className="task-list">Lista de Tarefas</h1>
+            <div className="input-group">
                 <input
                 type="text"
                 placeholder="Nova Tarefa"
                 value={novaTarefa}
                 onChange={(event) => setNovaTarefa (event.target.value)}
                 />
-                <button onClick={adicionarTarefa}>Adicionar</button>
+                <button className="add-button" onClick={adicionarTarefa}>Adicionar</button>
             </div>
             <ul>
                 {
                     tarefas.map((tarefa, index) => (
                         <li key={index}>
                             {tarefa}
+                            <button className="remove-button" onClick={() => removerTarefa(index)}>Remover Tarefa</button>
                         </li>
                     ))
                 }
@@ -48,7 +70,7 @@ export default function Home () {
                             tarefasFiltradas.map((tarefa, index) => (
                                 <li>{tarefa}</li>
                             ))
-                        ):(<p>Nenhuma Tarefa Encontrada</p>)}
+                        ):(<p className="empty-message" >Nenhuma Tarefa Encontrada</p>)}
                     </ul>
                 )
             }
